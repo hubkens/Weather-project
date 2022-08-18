0d959905c6df8db1b9ce1f105d9f97145f52e365
hubkens <ikenna_ihenatu@brown.edu>
2022-08-18 16:28:06 -0400

    working!

--- ts/stencil.js
+++ ts/stencil.js
@@ -1,1 +1,54 @@
 "use strict";
+let weather = {
+    apiKey: '7a6dc7c60556b4eeb40073b6ab1c0f58',
+    geocoding: function (searchInput) {
+        fetch('http:\\api.positionstack.com/v1/forward?access_key='
+            + this.apiKey + '&query=' + searchInput + '&fields=results.latitude,results.longitude,results.locality')
+            .then((response) => response.json())
+            .then((data) => this.retrieveForecastLink(data.data[0]))
+            .catch((error) => console.error('Error: Invalid Search Input,', error));
+    },
+    localityHolder: "",
+    retrieveForecastLink: function (geofeatures) {
+        const { latitude } = geofeatures;
+        const { longitude } = geofeatures;
+        const { locality } = geofeatures;
+        this.localityHolder = locality;
+        fetch('https://api.weather.gov/'
+            + parseFloat(latitude) + parseFloat(longitude))
+            .then((response) => response.json())
+            .then((data) => this.retrieveWeather(data.properties.forecast))
+            .catch((error) => console.error('Error: Invalid Search Input,', error));
+    },
+    retrieveWeather: function (link) {
+        fetch(link)
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data.properties.periods[0]))
+            .catch((error) => console.error('Error: Invalid Search Input,', error));
+    },
+    displayWeather: function (data) {
+        const { temperature } = data;
+        const { windSpeed } = data;
+        const { windDirection } = data;
+        const { icon } = data;
+        const { shortForecast } = data;
+        console.log(temperature, windSpeed, windDirection, icon, shortForecast);
+        const loc = document.querySelector(".location");
+        const ic = document.querySelector(".icon");
+        const win = document.querySelector(".wind");
+        const des = document.querySelector(".description");
+        const tem = document.querySelector(".temp");
+        loc.innerText = "Weather in " + this.localityHolder;
+        ic.src = icon;
+        win.innerText = windSpeed + " " + windDirection;
+        des.innerText = shortForecast;
+        tem.innerText = temperature + "°F";
+    },
+    search: function () {
+        const input = document.querySelector(".search-bar");
+        this.geocoding(input.value);
+    },
+};
+weather.geocoding('Baltimore');
+const search = document.querySelector("#search-button");
+search.addEventListener("click", function () { weather.search(); });
--- ts/stencil.ts
+++ ts/stencil.ts
@@ -1,29 +1,72 @@
-let features = JSON;
- function geocoding(){
-    const apiKey = "7a6dc7c60556b4eeb40073b6ab1c0f58";
-    const place = document.querySelector('#text');
-    fetch(
-        "http://api.positionstack.com/v1/forward?access_key=" 
-        + apiKey + "&query=" + place
-    )
+let weather = {
+    apiKey: '7a6dc7c60556b4eeb40073b6ab1c0f58',
+    geocoding : function(searchInput : string) {
+        fetch(
+            'http:\\api.positionstack.com/v1/forward?access_key='
+            + this.apiKey + '&query=' + searchInput + '&fields=results.latitude,results.longitude,results.locality')
+            .then((response) => response.json())
+            .then((data) => this.retrieveForecastLink(data.data[0]))
+            .catch((error: any) => console.error('Error: Invalid Search Input,', error));
+    },
+    localityHolder : "",
+    retrieveForecastLink: function(geofeatures : any) {
+        const {latitude} = geofeatures;
+        const {longitude} = geofeatures;
+        const {locality} = geofeatures;
+        this.localityHolder = locality;
+        fetch(
+            'https://api.weather.gov/'
+            + parseFloat(latitude) + parseFloat(longitude))
+            .then((response) => response.json())
+            .then((data) => this.retrieveWeather(data.properties.forecast))
+            .catch((error: any) => console.error('Error: Invalid Search Input,', error));
+    },
+    
+    retrieveWeather: function(link: string){
+        fetch(link)
         .then((response) => response.json())
-        .then((data) => features = data).then(() => console.log())
-}
-function retrieveForcast(){ 
-    //const lat = features['latitude']
-    return fetch("https://api.weather.gov/gridpoints/" + lat +"/" + long + "/forecast");
+        .then((data) => this.displayWeather(data.properties.periods[0]))
+        .catch((error: any) => console.error('Error: Invalid Search Input,', error));
+    },
+
+    displayWeather: function(data: any){
+        const { temperature } = data;
+        const { windSpeed } = data;
+        const { windDirection } = data;
+        const { icon } = data;
+        const { shortForecast } = data;
+        console.log(temperature, windSpeed, windDirection, icon, shortForecast);
+        
+        const loc = document.querySelector(".location") as HTMLElement | null;
+
+        const ic = document.querySelector(".icon") as HTMLImageElement | null;
+        
+        const win = document.querySelector(".wind")as HTMLElement | null;
+
+        const des = document.querySelector(".description")as HTMLElement | null;
+
+        const tem = document.querySelector(".temp")as HTMLElement | null;
+
+        loc!.innerText = "Weather in " + this.localityHolder;
+        ic!.src = icon;
+        win!.innerText = windSpeed + " " + windDirection;
+        des!.innerText = shortForecast;
+        tem!.innerText = temperature + "°F";
+        
+    },
+
+    search: function () {
+        const input = document.querySelector(".search-bar") as HTMLInputElement;
+        this.geocoding(input.value);
+    },
+
 }
 
-function displayForecast(){
+weather.geocoding('Baltimore');
+const search = document.querySelector("#search-button") as HTMLElement;
+search.addEventListener("click", function () {weather.search();})
 
-}
-let computeButton;
-window.onload = () => {
-    computeButton = document.getElementById('compute-button');
-    if (computeButton != null){
-        computeButton.addEventListener("click", () => displayForecast());
-    }  
-};
-    
-    
+
+        
+
 
